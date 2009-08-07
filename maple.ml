b24d3147bc5d90dec05e885c436d0f557471f9f1
herbelin <herbelin@0cf17b13-060f-0410-b1b1-c666bec9822a>
2009-08-07 12:12:00 +0000

    Mise en compatibilité suite uniformisation des noms de Libnames/Nametab (r12265/r12266).
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/coq-contribs/trunk@1007 0cf17b13-060f-0410-b1b1-c666bec9822a

--- maple.ml
+++ maple.ml
@@ -82,7 +82,7 @@ type expr =
 let constr_from dir s =
   let id = id_of_string s in
   try
-    constr_of_reference (Nametab.absolute_reference (make_path dir id))
+    constr_of_reference (Nametab.global_of_path (make_path dir id))
   with Not_found -> anomaly ("Could not find '"^s^"'.")
 
 (* Builds the constants of the Field reflexion structure *)
